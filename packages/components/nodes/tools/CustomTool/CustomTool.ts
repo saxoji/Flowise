2543d084bc3d5dfd97a6e5abc74b0551c2c140e5
Benedict JI <saxoji@gmail.com>
2024-08-06 17:20:36 +0900

    Add files via upload

--- packages/components/nodes/tools/CustomTool/CustomTool.ts
+++ packages/components/nodes/tools/CustomTool/CustomTool.ts
@@ -29,12 +29,6 @@ class CustomTool_Tools implements INode {
                 name: 'selectedTool',
                 type: 'asyncOptions',
                 loadMethod: 'listTools'
-            },
-            {
-                label: 'Return Direct',
-                name: 'returnDirect',
-                type: 'boolean',
-                optional: true
             }
         ]
         this.baseClasses = [this.type, 'Tool', ...getBaseClasses(DynamicStructuredTool)]
@@ -72,7 +66,6 @@ class CustomTool_Tools implements INode {
         const customToolName = nodeData.inputs?.customToolName as string
         const customToolDesc = nodeData.inputs?.customToolDesc as string
         const customToolSchema = nodeData.inputs?.customToolSchema as string
-        const customToolReturnDirect = nodeData.inputs?.returnDirect as boolean
 
         const appDataSource = options.appDataSource as DataSource
         const databaseEntities = options.databaseEntities as IDatabaseEntity
@@ -104,7 +97,7 @@ class CustomTool_Tools implements INode {
             let dynamicStructuredTool = new DynamicStructuredTool(obj)
             dynamicStructuredTool.setVariables(variables)
             dynamicStructuredTool.setFlowObject(flow)
-            dynamicStructuredTool.returnDirect = customToolReturnDirect
+
             return dynamicStructuredTool
         } catch (e) {
             throw new Error(e)
