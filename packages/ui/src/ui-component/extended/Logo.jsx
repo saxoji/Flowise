62740fd97e78cf1a692054bc5b24ad6842bd7a96
Benedict JI <saxoji@gmail.com>
2025-05-15 22:55:14 +0900

    Add files via upload

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,22 +1,19 @@
-import logo from '@/assets/images/flowise_white.svg'
-import logoDark from '@/assets/images/flowise_dark.svg'
-
+// SVG 가져오기에서 PNG 가져오기로 변경
+import logo from '@/assets/images/flowise_white.png' // .svg에서 .png로 변경
+import logoDark from '@/assets/images/flowise_dark.png' // .svg에서 .png로 변경
 import { useSelector } from 'react-redux'
 
 // ==============================|| LOGO ||============================== //
-
 const Logo = () => {
     const customization = useSelector((state) => state.customization)
-
     return (
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', marginLeft: '10px' }}>
             <img
                 style={{ objectFit: 'contain', height: 'auto', width: 150 }}
                 src={customization.isDarkMode ? logoDark : logo}
-                alt='Flowise'
+                alt='Linkbricks Horizon-AI'
             />
         </div>
     )
 }
-
 export default Logo
